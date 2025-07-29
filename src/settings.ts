6a0c81cb0b482e30fc500d60f884e0e83e988ea1
Bjarne Rentz <Bjarnerentz@live.de>
2025-07-29 19:29:54 +0200

    Improve default prompt

--- src/settings.ts
+++ src/settings.ts
@@ -5,6 +5,6 @@ export interface GeminiGeneratorSettings {
 }export const DEFAULT_SETTINGS: GeminiGeneratorSettings = {
 	apiKey: "",
 	removeHeadlineEnabled : false,
-	defaultPrompt: "Write a me an Obsidian Markdown Note without the Title on {TITLE}",
+	defaultPrompt: "Write a me an Obsidian Markdown Note for a Note with the title: {TITLE} \n You are writing directly in Obsidian thus you do not need an extra markdown environment. Also do not use any properties",
 };
 
